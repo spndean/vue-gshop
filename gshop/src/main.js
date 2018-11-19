3243fe0475c39e4842e0f0721db8966ab91940d9
liyixuan <13146744882@163.com>
2018-11-19 18:30:58 +0800

    first init

---
+++ gshop/src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/*
+ 入口JS
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import TopHeader from './components/TopHeader/TopHeader.vue'
+
+// 注册全局组件
+Vue.component('TopHeader', TopHeader);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+});
